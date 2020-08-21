0e1f7fb86bad4b10cd8f1b9ee480848ae0d7108c
Mostafa <mdmostafaalmahmud@gmail.com>
2020-08-21 17:42:20 +0600

    http library project upload to build restApi

---
+++ Http.js
@@ -0,0 +1,35 @@
+// Http library for REST Api usage
+class Http {
+    async get(url) {
+        const response = await fetch(url).then(data => data.json());
+        return response;
+    }
+    async post(url, data) {
+        const response = await fetch(url, {
+            method: 'POST',
+            body: JSON.stringify(data),
+            headers: {
+                "Content-Type": "application/json; charset=UTF-8"
+            }
+        }).then(data => data.json());
+        return response;
+    }
+    async update(url, data) {
+        const response = await fetch(url, {
+            method: 'PUT',
+            body: JSON.stringify(data),
+            headers: {
+                "Content-Type": "application/json; charset=UTF-8"
+            }
+        }).then(data => data.json());
+        return response;
+    }
+    async delete(url) {
+        const response = await fetch(url, {
+            method: 'DELETE'
+        }).then(data => data.json());
+        return response;
+    }
+}
+
+// export const http = new Http();
---
+++ README.md
@@ -0,0 +1,4 @@
+# http-library-restAPI
+You can build your own rest API with this http library.
+Just clone this repo and use it as you want.
+Happy programming!
